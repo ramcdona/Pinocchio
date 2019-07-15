7cb76282e72c4866a596a7b1ff920398f830830b
Rob McDonald <rob.mcdonald@uber.com>
2019-07-15 16:49:10 -0700

    #ifdef out Windows DLL specific code.

--- Pinocchio/Pinocchio.cpp
+++ Pinocchio/Pinocchio.cpp
@@ -18,13 +18,17 @@
 
 // Pinocchio.cpp : Defines the entry point for the DLL application.
 
+#ifdef WIN32
 #include "windows.h"
+#endif
+
 #include "Pinocchio.h"
 
 #ifdef _MANAGED
 #pragma managed(push, off)
 #endif
 
+#ifdef WIN32
 BOOL APIENTRY DllMain( HMODULE hModule,
 DWORD  ul_reason_for_call,
 LPVOID lpReserved
@@ -40,7 +44,7 @@ LPVOID lpReserved
   }
   return TRUE;
 }
-
+#endif
 
 #ifdef _MANAGED
 #pragma managed(pop)
